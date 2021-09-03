8b37b214e91f8323ba9a01bf222cf83b8f00ae96
daidaros08 <daidaros08@gmail.com>
2021-09-03 18:19:41 -0300

    Update Promocion.java

--- src/TurismoenlaTierraMedia/Promocion.java
+++ src/TurismoenlaTierraMedia/Promocion.java
@@ -1,6 +1,32 @@
  package TurismoenlaTierraMedia;
 
-public class Promocion {
+import java.util.LinkedList;
+
+public class Promocion extends Producto{
+	private LinkedList <Atracciones> atraccionesIncluidas;
+
+	public Promocion(Atracciones atraccion1, Atracciones atraccion2, Atracciones atraccion3) {
+		super (0.0);
+		this.atraccionesIncluidas.add(atraccion1);
+		this.atraccionesIncluidas.add(atraccion2);
+		this.atraccionesIncluidas.add(atraccion3);
+	
+	}
+	
+	public Promocion(Atracciones atraccion1, Atracciones atraccion2) {
+		super (0.0);
+		this.atraccionesIncluidas.add(atraccion1);
+		this.atraccionesIncluidas.add(atraccion2);
+	
+	}
+	
+	public Promocion(Double costoPromo, Atracciones atraccion1, Atracciones atraccion2, Atracciones atraccion3) {
+		super (costoPromo);
+		this.atraccionesIncluidas.add(atraccion1);
+		this.atraccionesIncluidas.add(atraccion2);
+		this.atraccionesIncluidas.add(atraccion3);
+	
+	}
 
 	
 	
