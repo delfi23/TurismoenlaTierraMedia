b22b83a1498e7be9f880405d9719208bc126497e
daidaros08 <daidaros08@gmail.com>
2021-09-03 18:13:05 -0300

    Update Producto.java

--- src/TurismoenlaTierraMedia/Producto.java
+++ src/TurismoenlaTierraMedia/Producto.java
@@ -1,5 +1,16 @@
 package TurismoenlaTierraMedia;
 
 public class Producto {
-
+	private Double costo;
+	
+	public Producto (Double costo) {
+		this.setCosto(costo);
+	}
+	
+	public void setCosto (Double costo) {
+		this.costo = costo;
+	}
+	public Double getCosto () {
+		return this.costo;
+	}
 }
